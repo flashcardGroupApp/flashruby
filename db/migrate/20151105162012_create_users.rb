469a5fef294a4032e756c5e408b7dc76f21ded90
Joe Winter <joe@joeventures.com>
2015-11-05 11:30:33 -0500

    Added User model. So there.

---
+++ db/migrate/20151105162012_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :username, null: false
+      t.string :full_name
+      t.string :email, null: false
+      t.string :auth_token
+      t.string :password_digest
+
+      t.timestamps null: false
+    end
+  end
+end
